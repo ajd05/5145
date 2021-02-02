89f89b1c114bcc42ee8bf9b65517a8ec9384b11f
ajd05 <adaodu@uncc.edu>
2021-02-02 12:16:48 -0500

    activity programming done

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,10 @@
-int main () {
+#include <iostream>
+#include <unistd.h>
 
+int main () {
+  char hostname[20];
+  int hnlen = 19;
+  gethostname(hostname, hnlen);
+  printf("Hostname is %s \n", hostname);
   return 0;
 }
